640cfe0cdd313f69eaaf7360608467606d180940
Slavik <slavikmbm@gmail.com>
2021-06-08 12:05:49 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+"use stript"
+
+let namber = 5;
+const lrftBordtrdWidth = 1;
+
+namber =10;
+console.log(namber);
+
+var a = 5;
+console.log(-4/0);
+
+const persone = '5';
+
+const obj ={
+    name: "John",
+    age: 25,
+}
+
+var a =20;
+
+console.log(a);
